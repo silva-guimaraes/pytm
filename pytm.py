056eaf901fa3d35c2be0d05d0f97d55c65c6d373
silva guimaraes <silva__guimaraes@hotmail.com>
2022-10-31 23:09:01 -0300

    separar funções

--- pytm.py
+++ pytm.py
@@ -3,22 +3,32 @@
 # pytm: Parse YouTube Music
 
 from bs4 import BeautifulSoup
-import yt_dlp as ydl
+import shutil
+import yt_dlp
 import requests
 import sys
 import re
 import os
 
-rym = ""
-album_title = ""
+# todo: classes para cada site diferente
 
-if len(sys.argv) >= 2:
-    video_url = sys.argv[1]
-else: 
-    print('video?')
-    exit(1)
-if len(sys.argv) == 3:
-    rym = sys.argv[2]
+# rym automaticamente bloqueia qualquer ip com o user-agent padrão do requests.get()
+# favor não fazer nenhum request sem antes especificar esse header
+headers = {'sec-ch-ua': '\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\",\
+\"Google Chrome\";v=\"101\"',
+        'sec-ch-ua-mobile': '?0',
+        # 'sec-ch-ua-platform': '\"Linux\"',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
+(KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,\
+image/avif,image/webp, image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Sec-Fetch-Dest': 'document',
+        'Accept-Encoding': 'gzip, deflate, br'}
 
 class track:
     # artist = []
@@ -39,34 +49,20 @@ class track:
             self.position, self.title, self.start, self.end, self.end - self.start)
 
 
-track_list = []
-if rym:
-    # rym automaticamente bloqueia qualquer ip com o user-agent padrão do requests.get()
-    headers = {'sec-ch-ua': '\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\",\
-    \"Google Chrome\";v=\"101\"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '\"Linux\"',
-            'DNT': '1',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
-    (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,\
-    image/avif,image/webp, image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Accept-Encoding': 'gzip, deflate, br'}
+def extract_rym(rym_url):
     try:
-        print('pedido para {}...'.format(rym))
-        rym_page = requests.get(rym, headers=headers)
+        print('pedido para {}...'.format(rym_url))
+        rym_page = requests.get(rym_url, headers=headers)
     except requests.exceptions.ConnectionError:
         print('falha em estabelecer nova conexão. saindo e sinalizando como erro.',
             file=sys.stderr)
         exit(1)
 
+    track_list = []
     rym_page = BeautifulSoup(rym_page.text, 'html.parser')
-    album_title = rym_page.find_all('div', {'class': 'album_title'})[0].contents[0].strip()
+    album_title = rym_page.select('#column_container_right > div.section_main_info\
+.section_outer > div > div.album_title')[0].contents[0].string.strip()
+    # album_title = rym_page.find_all('div', {'class': 'album_title'})[0].contents[0].strip()
     print(album_title)
     track_starts = 0
     tracks = rym_page.find(id='tracks')
@@ -83,26 +79,7 @@ if rym:
         track_list.append(new_track)
         track_starts += duration
 
-options = ydl.YoutubeDL(
-    {'format': 'mp4',
-    'allow_multiple_audio_stream': False,
-    'allow_multiple_video_stream': False
-    })
-
-chapters = []
-result = {}
-with options as ydl:
-    result = ydl.extract_info(
-        video_url,
-        download=False)
-    chapters = result['chapters']
-    if chapters or rym:
-        result = ydl.extract_info(
-            video_url,
-            download=True)
-    elif not rym:
-        print('vídeo não possui capítulos.')
-        exit(1)
+    return track_list, album_title, rym_page
 
 # / (barra) é o denominador de diretórios no linux e mac e \ (contra barra) no windows
 # nenhuma faixa pode ter nenhum desses dois caracteres então nós trocamos por algum outro
@@ -113,36 +90,91 @@ def replace_slash(s):
     return s
 # todo: salva o nome original da faixa nos metadados
 
-def splice_video(basename, start, end):
-    video_path = result['requested_downloads'][0]['filepath']
-    filename = replace_slash(basename) + ".mp3"
+def create_album_dir(metadata: dict, album_title: str) -> str:
+    yt_video_title = ""
+
     if album_title:
         yt_video_title = replace_slash(album_title)
     else:
-        yt_video_title = replace_slash(result['title'])
+        yt_video_title = replace_slash(metadata['title'])
+
     if not os.path.exists(yt_video_title):
         os.makedirs(yt_video_title)
+
+    return yt_video_title
+
+    
+def splice_video(metadata, yt_video_title, basename, start, end):
+    video_path = metadata['requested_downloads'][0]['filepath']
+    filename = replace_slash(basename) + ".mp3"
     output_path = os.path.join(yt_video_title, filename)
     print('[pytm] enconding: {}...'.format(basename))
     command = 'ffmpeg -hide_banner -loglevel error -ss "{}" -to "{}" -i "{}" -vn "{}"'
     os.system(command.format(start, end, video_path, output_path))
 
-
-def split_chapters(chapters):
+def split_chapters(metadata, yt_video_title, chapters):
     for i in chapters:
-        splice_video(i['title'], i['start_time'], i['end_time'])
+        splice_video(metadata, yt_video_title, i['title'], i['start_time'], i['end_time'])
 
 
-def split_duration(track_list):
+def split_duration(metadata, yt_video_title, track_list):
     for i in track_list:
-        splice_video('{:02d}. {}'.format(i.position, i.title),
+        splice_video(metadata, yt_video_title, '{:02d}. {}'.format(i.position, i.title),
                      i.start, i.end) 
         
+def download_cover(album_dir, url):
+    local_filename = os.path.join(album_dir,'cover.png')
+    with requests.get(url, stream=True, headers=headers) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
 
-if rym:
-    split_duration(track_list)
-else:
-    split_chapters(chapters)
+def main():
+    track_list = []
+    album_title = ""
+    video_url = ""
+    rym_url = ""
+    album_dir = ""
+    
+    if len(sys.argv) >= 2:
+        video_url = sys.argv[1]
+    else: 
+        print('video?')
+        exit(1)
+    if len(sys.argv) == 3:
+        rym_url = sys.argv[2]
+        track_list, album_title, rym_page = extract_rym(rym_url)
+
+    options = yt_dlp.YoutubeDL(
+        {'format': 'mp4',
+        'allow_multiple_audio_stream': False,
+        'allow_multiple_video_stream': False
+        })
+
+    chapters = []
+    metadata = {}
+    with options as ydl:
+        metadata = ydl.extract_info(video_url, download=False)
+        chapters = metadata['chapters']
+        if chapters or track_list:
+            metadata = ydl.extract_info(video_url, download=True)
+        elif not rym:
+            print('vídeo não possui capítulos.')
+            exit(1)
+
+    yt_video_title = create_album_dir(metadata, album_title)
+    if track_list:
+        split_duration(metadata, yt_video_title, track_list)
+        album_link = rym_page.select('#column_container_left > div.page_release_art_frame\
+ > div > a > div > img')[0]['src']
+        download_cover(yt_video_title, 'https:' + album_link)
+    else:
+        split_chapters(metadata, chapters)
+
+    # os.remove(filepath)
+
+def teste(foo: int, bar: str):
+    return 'bar: {}'.format(foo)
 
 
-# os.remove(filepath)
+if __name__ == "__main__":
+    main()
